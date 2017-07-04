c0bc3eef6ae6aacc586dfdcde06f53ec0f4f55fa
vishnu-v-v <ng.pingme@gmail.com>
2017-07-05 00:16:19 +0530

    Add state props properties in jsx template

--- App.jsx
+++ App.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 
 class App extends React.Component {
+   constructor() {
+      super();
+
+      this.state = {
+         data:
+         [
+            {
+               "id":1,
+               "name":"Foo",
+               "age":"20"
+            },
+            {
+               "id":2,
+               "name":"Bar",
+               "age":"30"
+            },
+            {
+               "id":3,
+               "name":"Baz",
+               "age":"40"
+            }
+         ]
+      }
+   }
+
    render() {
       return (
          <div>
             <Header/>
-            <Content/>
+            <table>
+               <tbody>
+                  {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+               </tbody>
+            </table>
          </div>
       );
    }
@@ -21,13 +50,14 @@ class Header extends React.Component {
    }
 }
 
-class Content extends React.Component {
+class TableRow extends React.Component {
    render() {
       return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
-         </div>
+         <tr>
+            <td>{this.props.data.id}</td>
+            <td>{this.props.data.name}</td>
+            <td>{this.props.data.age}</td>
+         </tr>
       );
    }
 }
