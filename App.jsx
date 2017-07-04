c4dc09a453ba198449534e8375a6dd717a7a3e23
vishnu-v-v <ng.pingme@gmail.com>
2017-07-04 23:58:15 +0530

    Add different classes and use it inside root class

--- App.jsx
+++ App.jsx
@@ -4,9 +4,29 @@ class App extends React.Component {
    render() {
       return (
          <div>
+            <Header/>
+            <Content/>
+         </div>
+      );
+   }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
             <h1>Header</h1>
-            {/*End of the line Comment...*/}
-            {/*Multi line comment...*/}
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
          </div>
       );
    }
