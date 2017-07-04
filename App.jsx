ceb7f755b48ddaa6c9d39ea444da817bb6965794
vishnu-v-v <ng.pingme@gmail.com>
2017-07-04 23:54:12 +0530

    Add comments inside jsx components

--- App.jsx
+++ App.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
-
-      var myStyle = {
-         fontSize: 100,
-         color: '#FF0000'
-      }
-
       return (
          <div>
-            <h1 style = {myStyle}>Header</h1>
+            <h1>Header</h1>
+            {/*End of the line Comment...*/}
+            {/*Multi line comment...*/}
          </div>
       );
    }
